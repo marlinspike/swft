bcd8797fbc22b41bb13661cd0301cb3210caf35a
Reuben Cleetus <rcleetus@gmail.com>
2025-06-22 14:38:04 -0400

    updated workflow

--- app/main.py
+++ app/main.py
@@ -40,4 +40,4 @@ async def get_time():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=80, reload=True)
