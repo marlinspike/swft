ae69de020a172411d8896e2514dbb58f7563339a
Matthew Collera <macoller@microsoft.com>
2025-10-23 10:18:49 -0400

    removed bad package

--- python/install-vulnerable.bat
+++ python/install-vulnerable.bat
@@ -8,7 +8,8 @@ python -m pip install --upgrade pip
 
 echo Installing core vulnerable packages...
 python -m pip install fastapi==0.68.0
-python -m pip install jinja2==2.11.0  
+python -m pip install jinja2==2.11.0
+python -m pip install markupsafe==1.1.1
 python -m pip install flask==1.1.4
 python -m pip install pydantic==1.8.2
 python -m pip install requests==2.25.1
@@ -17,6 +18,7 @@ python -m pip install uvicorn==0.15.0
 python -m pip install click==7.1.2
 python -m pip install werkzeug==1.0.1
 python -m pip install setuptools==65.5.0
+python -m pip install django==3.1.14
 
 echo Installing additional dependencies...
 python -m pip install python-dotenv pytest pytest-cov httpx
